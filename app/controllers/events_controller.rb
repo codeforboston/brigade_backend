5280b3a7af440a376fec9693f208becc8785f509
Liam Morley <liam@carpeliam.com>
2014-01-18 21:55:39 -0500

    add support for fetching events

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,7 +1,15 @@
 class EventsController < ApplicationController
   respond_to :html, :json
 
+  def index
+    respond_with(@events = Event.all)
+  end
+
   def create
     respond_with(@event = Event.create(params[:event]))
+  end
+
+  def show
+    respond_with(@event = Event.find(params[:id]))
   end
 end
